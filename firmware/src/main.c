4fa935a3c5a3b512c63c64b8c965d3dbdbaf1c3d
Brian Tate <brian.tate@microchip.com>
2023-05-06 19:02:51 -0500

    update main to use PIR driver

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -28,31 +28,11 @@
 #include "definitions.h"                // SYS function prototypes
 #include "MCP3221.h"
 #include "analog/MCP3221.h"
+#include "PIR_CLICK.h"
 
 #define MOTION_DETECT_THRESHOLD (2500)
 
 #define ADC_METHOD_MCP3221
-
-mcp3221Handle mcp3221;
-
-bool isConversionComplete;
-
-void MyMCP3221_Callback(MCP3221_EVENT event)
-{
-    if (MCP3221_EVENT_CONVERSION_COMPLETE == event)
-    {
-        isConversionComplete = true;
-    }
-    else if(MCP3221_EVENT_CONVERSION_PENDING == event)
-    {
-        printf("Conversion pending \r\n");
-    }
-    else
-    {
-        printf("mcp3221 error: %d \r\n", event);
-    }
-}
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -65,20 +45,10 @@ int main ( void )
 {
     /* Initialize all modules */
     SYS_Initialize ( NULL );
-    isConversionComplete = false;
     ADC0_Enable();
     puts("PIR presence detection\r\n");
     
-    mcp3221 = MCP3221_Open(0x4D, MyMCP3221_Callback);
-    
-    if(0 == mcp3221)
-    {
-        puts("error opening MCP3221\r\n");
-    }
-    else
-    {
-        printf("MCP3221 open successful %x \r\n", mcp3221);
-    }
+    PIR_Init();
     
     while ( true )
     {
@@ -95,23 +65,8 @@ int main ( void )
         LED0_Toggle();
         
 #ifdef ADC_METHOD_MCP3221
-        MCP3221_ConversionStart(mcp3221);
-        while(!MCP3221_ConversionStatusGet(mcp3221))
-        {
-            __asm("NOP");
-        }
-
-        if(MCP3221_EVENT_CONVERSION_COMPLETE == MCP3221_ConversionStatusGet(mcp3221))
-        {    
-            mcpVal = MCP3221_ConversionResultGet(mcp3221);
-//            printf("val = %d \r\n", mcpVal);
-        }
-        else
-        {
-            puts("MCP3221 conversion error\r\n");
-        }
         
-        if(MOTION_DETECT_THRESHOLD < mcpVal)
+        if(PIR_IsPresent())
         {
             puts("MCP motion detected \r\n");
         }
